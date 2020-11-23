e04537fc3c06a349bac96ce8ab339e7efe4da39b
unknown <4lvaroo.up@gmail.com>
2020-11-23 09:48:58 -0300

    ADD: new files added

---
+++ index.js
@@ -0,0 +1,25 @@
+// Material Select Initialization
+$("#demo").easySelect({
+    buttons: true,
+    search: true,
+// placeholder text
+  placeholder: 'Select item',
+
+  // color of selected options
+  selectColor: '#414c52',
+
+  // color of placeholder text
+  placeholderColor: '#838383',
+
+  // title of selected options
+  itemTitle: 'Selected items',
+
+  // shows values
+  showEachItem: false,
+
+  // width
+  width: '100%',
+
+  // max height
+  dropdownMaxHeight: 'auto'
+  })
